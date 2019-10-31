143415206dd8a8776ba7f54c2d5da3e37d6ea346
earrobo <estalinarrobo@gmail.com>
2019-10-31 10:45:00 -0500

    Creación proyecto
    Creación de primer proyecto

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function conectar_mysql(){
+    $server='localhost';
+    $user='root';
+    $password='';
+    $db='paises';
+    $conectar=mysqli_connect($server,$user,$password,$db);
+    if(!$conectar){
+        die("Error al conectar con la bd");
+    }
+    return $conectar;
+}
+
+function get_list(){
+$retval='';
+$retval='
+<table align="center" border=1>
+		<tr bgcolor=#E3CEF6>
+			<td colspan=7 align="center"><b>SISTEMA DE PAISES</b></td>
+		</tr>
+		<tr bgcolor=#D8CEF6>
+			<td colspan=7><a href=""><img src="images/new.png"></a></td>
+		</tr>
+		<tr bgcolor=#FAAC58>
+			<td align="center"><b>Codigo pais:</b></td>
+			<td align="center"><b>Nombre:</b></td>
+        </tr>';
+        $conectar=conectar_mysql();
+        $sql='SELECT * FROM paises;';
+        $res=mysqli_query($conectar,$sql);
+        while($pais = mysqli_fetch_array($res)){
+           $retval.=' 
+		<tr bgcolor=#CEE3F6>
+			<td>'.$pais['cod_pais'].'</td>
+			<td align="center">'.$pais['nombre'].'</td>
+            <td><a href="index.php?op=edi&id='.$pais['id'].'">
+            <img src="images/edit.png"></a></td>
+            <td><a href="index.php?op=bor&id='.$pais['id'].'">
+            <img src="images/borrar.png"></a></td>
+        </tr>';
+        }
+        $retval.='</table>';
+        return $retval;
+}
+
+echo get_list();
+?>
+</body>
+</html>
